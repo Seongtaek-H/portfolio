336e3074ec5806c5c4606900af57c731d7a02b9a
seongtaek <hst1993@hanmail.net>
2022-02-09 17:44:43 +0900

    JS:navbar scrolling

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar__scroll");
   }
 });
+
+// navbar menu scrolling
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
